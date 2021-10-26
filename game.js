88f42619944f92fe8b214a4c6ac29626af5d782d
imsern <kristofferims@hotmail.com>
2021-10-26 12:21:38 +0200

    update gameover

--- game.js
+++ game.js
@@ -1,45 +1,48 @@
-import { update as updateSnake, draw as drawSnake, snakeSpeed, getSnakeHead, snakeIntersection } from './snake.js'
-import { update as updateFood, draw as drawFood } from './food.js'
-import { outsideGrid } from './grid.js'
-
+import {
+	update as updateSnake,
+	draw as drawSnake,
+	snakeSpeed,
+	getSnakeHead,
+	snakeIntersection,
+} from "./snake.js";
+import { update as updateFood, draw as drawFood } from "./food.js";
+import { outsideGrid } from "./grid.js";
 
 let lastRenderTime = 0;
 let gameOver = false;
-const gameBoard = document.getElementById('board');
+const gameBoard = document.getElementById("board");
 
-function main(currentTime){
-    if (gameOver) {
-        if (confirm('You lost. Press OK to restart.')){
-            window.location = '/';
-        }
-        return
-    }
+function main(currentTime) {
+	if (gameOver) {
+		if (confirm("You lost. Press OK to restart.")) {
+			window.location.href = "https://imsern.github.io/snakeGame/";
+		}
+		return;
+	}
 
+	window.requestAnimationFrame(main);
+	const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+	if (secondsSinceLastRender < 1 / snakeSpeed) return;
 
-    window.requestAnimationFrame(main);
-    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
-    if (secondsSinceLastRender < 1 / snakeSpeed) return;
-    
-    lastRenderTime = currentTime;
-    
+	lastRenderTime = currentTime;
 
-    updateView();
-    draw();
+	updateView();
+	draw();
 }
 window.requestAnimationFrame(main);
 
-function updateView(){
-    updateSnake();
-    updateFood();
-    checkDeath();
+function updateView() {
+	updateSnake();
+	updateFood();
+	checkDeath();
 }
 
-function draw(){
-    gameBoard.innerHTML = ''
-    drawSnake(gameBoard);
-    drawFood(gameBoard);
+function draw() {
+	gameBoard.innerHTML = "";
+	drawSnake(gameBoard);
+	drawFood(gameBoard);
 }
 
-function checkDeath(){
-    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
+function checkDeath() {
+	gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
 }
